09dcb55d1312530b1dff35631431515fd7088359
micowavedram <microwavedram@gmail.com>
2026-07-27 21:45:03 +0100

    [no-ci] make the library not jdk25 hungry

--- Protocol/build.gradle.kts
+++ Protocol/build.gradle.kts
@@ -4,13 +4,17 @@ plugins {
 }
 
 group = "io.github.openboatutils"
-version = "0.0.6"
+version = "0.0.7"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.release.set(21)
+}
 
 publishing {
     publications {
