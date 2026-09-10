c0ea7c41f5bc8a6c746fecd3edf49aa240b4755b
micowavedram <microwavedram@gmail.com>
2026-09-10 01:11:51 +0100

    manually remap 1.21.9+ for mixins

--- src/main/java/dev/o7moon/openboatutils/GetStepHeight.java
+++
@@ -1,8 +0,0 @@
-package dev.o7moon.openboatutils;
-
-import org.spongepowered.asm.mixin.Unique;
-
-public interface GetStepHeight {
-    @Unique
-    float openboatutils$getStepHeight();
-}
--- src/main/java/dev/o7moon/openboatutils/ScaledBoatRenderState.java
+++
@@ -1,6 +0,0 @@
-package dev.o7moon.openboatutils;
-
-public interface ScaledBoatRenderState {
-    float openBoatUtils$getScale();
-    void openBoatUtils$setScale(float scale);
-}
